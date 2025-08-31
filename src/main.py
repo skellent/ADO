26c0ed8bfab720bea1d7d8847af688914ff883c9
skellent <skellent69@gmail.com>
2025-08-31 17:44:04 -0400

    Manejo de Base de Datos

--- src/main.py
+++ src/main.py
@@ -3,34 +3,123 @@ import streamlit as st
 
 # Importacion de Rich para mejorar la depuracion
 from rich import print
+from time import sleep as esperar
 
 # Importacion de Modulos
-import modules.configuracion as configuracion
+import modules.configuracion as Configuracion
 import modules.database as ADOdataBase
 
-# Obtencion de la Configuracion
-CONSTANTES = configuracion.cargar_constantes()
-DEBUG = CONSTANTES["desarrollo"]["debug"]
-if DEBUG:
-    print("[bold green]EL MODO DEBUG ESTA ACTIVO, TODAS LAS IMPRESIONES APARECERAN EN LA TERMINAL[/bold green]")
-    print("[bold blue]Secretos Cargados:[/bold blue]")
-    print(st.secrets)
+def configuracion() -> int:
+    # Obtencion de la Configuracion
+    try:
+        CONSTANTES = Configuracion.cargar_constantes()
+        DEBUG = CONSTANTES["desarrollo"]["debug"]
+        if DEBUG:
+            print("[bold green]EL MODO DEBUG ESTA ACTIVO, TODAS LAS IMPRESIONES APARECERAN EN LA TERMINAL[/bold green]")
+            print("[bold blue]Secretos Cargados:[/bold blue]")
+            print(st.secrets)
+        # Configuracion global de la aplicacion
+        st.set_page_config(
+            page_title=CONSTANTES["general"]["titulo"],
+            page_icon=CONSTANTES["general"]["icono"],
+            layout=CONSTANTES["general"]["wide"],
+            initial_sidebar_state=CONSTANTES["general"]["sidebar_estado"],
+        )
+        # Configuracion del SideBar
+        st.logo(
+            image=CONSTANTES["general"]["logotipo"],
+            icon_image=CONSTANTES["general"]["logo"]
+        )
+    except Exception as e:
+        if DEBUG: print(f"[bold red]Error al cargar la configuracion: {e}[/bold red]")
+        return 1
+    return 0
 
-# Configuracion global de la aplicacion
-st.set_page_config(
-    page_title=CONSTANTES["general"]["titulo"],
-    page_icon=CONSTANTES["general"]["icono"],
-    layout=CONSTANTES["general"]["wide"],
-    initial_sidebar_state=CONSTANTES["general"]["sidebar_estado"],
-)
+# Corroborar estado del DEBUG
+def DebugEstado() -> bool | str:
+    # Obtencion de la Configuracion
+    CONSTANTES = Configuracion.cargar_constantes()
+    DEBUG = CONSTANTES["desarrollo"]["debug"]
+    return DEBUG
 
-# Configuracion del SideBar
-st.logo(
-    image=CONSTANTES["general"]["logotipo"],
-    icon_image=CONSTANTES["general"]["logo"]
-)
+# Se corrobora la conexion a la base de datos mediante la obtencion de las tablas
+# Si hay menos de 1 tabla, la base no esta completamente creada y el programa
+# No esta completamente instalado, asi que hay que crear la tabla de usuarios
+# y registrar al usuario admin
+def BaseDatosEstado(bd: dict, debug: bool = False) -> int:
+    tablas = ADOdataBase.ObtenerTablas(bd, debug=debug)
+    if type(tablas) == list: # Devolvio una lista vacia, conexion exitosa
+        if len(tablas) > 0: # Verifica si hay tablas en la base, de lo contrario devuelve 1 indicando instalacion pendiente
+            if ADOdataBase.ObtenerRegistrosDeTabla(bd, "usuarios", debug=debug) == "ERROR" or ADOdataBase.ObtenerRegistrosDeTabla(bd, "usuarios", debug=debug) == []:
+                return 1 # La tabla de usuarios no existe, por lo tanto la base de datos no esta completamente creada
+            return 0
+        return 1
+    elif type(tablas) == str and tablas == "ERROR":
+        # Hubo un error en la consulta, esto solo ocurre si la base de datos no existe o hay un error en la conexion
+        return "ERROR"
+    return 1
 
-# Prueba de la conexion a la base de datos mediante consulta directa
-if DEBUG: print("[bold blue]Probando la conexion a la base de datos...[/bold blue]")
-registros = ADOdataBase.ObtenerRegistrosDeTabla(st.secrets, tabla="testing", debug=DEBUG)
-print(registros)
+# Declaracion de funcion Principal
+def main() -> None:
+
+    # Configura la pagina
+    if configuracion(): # si se cumple la condicion, hubo un error
+        st.error("Error al cargar la configuracion, revise la terminal para mas detalles")
+        st.stop()
+
+    # Estado del DEBUG
+    if 'DEBUG' not in st.session_state:
+        st.session_state['DEBUG'] = DebugEstado() # Es imposible que falle, ya que si la configuracion falla, la funcion principal no se ejecuta
+    if st.session_state['DEBUG']: st.write(f"DEBUG: {st.session_state['DEBUG']}")
+
+    # Estado de la Base de Datos
+    estadoBD = BaseDatosEstado(st.secrets, debug=st.session_state['DEBUG'])
+    if estadoBD == "ERROR": # si se cumple la condicion, hubo un error
+        st.error("Hubo un problema al conectar con la base de datos, contacte con soporte")
+        # Debido a que la base de datos no esta creada, no se puede continuar
+        st.stop()
+    elif estadoBD == 1: # La base de datos no esta completamente creada
+        st.warning("El Software no esta completamente instalado, por favor complete la instalacion")
+        # Si la instalacion no esta completa, significa que no hay usuarios en la base de datos
+        # Por lo tanto se debe crear la tabla de usuarios y registrar al administrador
+        
+        if ADOdataBase.CrearTablaUsuarios(st.secrets, debug=st.session_state['DEBUG']): # Si se cumple la condicion, hubo un error
+            st.error("Error al crear la tabla de usuarios, contacte con soporte")
+            st.stop()
+        if st.session_state['DEBUG']: st.success("Tabla de usuarios creada exitosamente")
+        
+        # Ahora se crea un formulario para registrar al usuario administrador
+        with st.form("registro_admin", clear_on_submit=True):
+            st.subheader("Registro de Usuario Administrador")
+            col1, col2 = st.columns(2)
+            with col1:
+                usuario = st.text_input("Usuario", max_chars=20, help="Nombre de usuario del administrador")
+            with col2:
+                contrasena = st.text_input("Contraseña", type="password", max_chars=20, help="Contraseña del administrador")
+            
+            # Se procede con el registro
+            if st.form_submit_button("Registrar Administrador", type="primary", use_container_width=True, help="Al presionar este boton se registrara el usuario administrador, esta accion no se puede deshacer"):
+                try:
+                    if usuario != "" or contrasena != "" and len(usuario) > 4 and len(contrasena) > 7: # Verificar los campos
+                        if ADOdataBase.AñadirUsuario(st.secrets, usuario, contrasena, 0, debug=st.session_state['DEBUG']): # Si se cumple la condicion, hubo un error
+                            st.error("Error al registrar el usuario administrador, contacte con soporte")
+                            st.stop()
+                        st.success("Usuario administrador registrado exitosamente, seras redirigido a la pagina de inicio de sesion en un momento")
+                        st.balloons()
+                        # Breve pantalla de carga antes de recargar la pagina para animar
+                        carga = st.progress(0)
+                        for i in range(100):
+                            esperar(0.02)
+                            carga.progress(i + 1)
+                        st.rerun()
+                    else:
+                        st.error("Error al registrar el usuario administrador, por favor verifique los campos")
+                except Exception as e:
+                    print(e)
+    
+    else: # La base de datos esta completamente creada y lista para usarse al igual que el programa
+        st.success("El Software esta completamente instalado y listo para usarse")
+
+# Ejecucion de la funcion Principal
+if __name__ == "__main__":
+    main()
--- src/modules/database.py
+++ src/modules/database.py
@@ -12,7 +12,7 @@ import mariadb
 import sys
 
 # Funcion para conectar a la base de datos
-def ConectarBD(usuario: str, contrasena: str, direccion: str, puerto: str, base_datos: str, debug: bool = False):
+def ConectarBD(usuario: str, contrasena: str, direccion: str, puerto: str, base_datos: str, debug: bool = False) -> mariadb.Connection:
     try:
         conn = mariadb.connect(
             user=usuario,
@@ -32,7 +32,7 @@ def ConectarBD(usuario: str, contrasena: str, direccion: str, puerto: str, base_
         sys.exit(1)
 
 # Funcion para crear un cursor
-def CrearCursor(conector, debug: bool = False):
+def CrearCursor(conector, debug: bool = False) -> mariadb.Cursor:
     try:
         cursor = conector.cursor()
         if cursor:
@@ -40,21 +40,78 @@ def CrearCursor(conector, debug: bool = False):
             return cursor
         else:
             if debug: print("[bold red]Error al crear el cursor[/bold red]")
-            sys.exit(1)
     except mariadb.Error as e:
         if debug: print(f"[bold red]Error al crear el cursor: {e}[/bold red]")
-        sys.exit(1)
 
 # Funcion para cerrar la conexion
-def CerrarConexion(conector, debug: bool = True):
+def CerrarConexion(conector, debug: bool = True) -> None:
     try:
         conector.close()
         if debug: print("[bold green]Conexion cerrada exitosamente[/bold green]")
     except mariadb.Error as e:
         if debug: print(f"[bold red]Error al cerrar la conexion: {e}[/bold red]")
 
+# Funcion para crear la tabla de usuarios
+def CrearTablaUsuarios(bd: dict, debug: bool = False) -> int:
+    if debug: print(bd)
+    cursor = CrearCursor(
+        ConectarBD(
+            bd["usuario"],
+            bd["contrasena"],
+            bd["direccion"],
+            bd["puerto"],
+            bd["base_datos"],
+            debug=debug
+        ),
+        debug=debug
+    )
+    try:
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS usuarios (
+                id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                usuario VARCHAR(20) NOT NULL,
+                contrasena VARCHAR(20) NOT NULL,
+                tipo INT NOT NULL,
+                fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                CHECK (LENGTH(usuario) >= 4),
+                CHECK (LENGTH(contrasena) >= 8)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+            """)
+        cursor.connection.commit()
+        if debug: print(f"[bold green]Tabla de usuarios creada exitosamente[/bold green]")
+        return 0
+    except mariadb.Error as e:
+        if debug: print(f"[bold red]Error al crear la tabla de usuarios: {e}[/bold red]")
+    finally:
+        CerrarConexion(cursor.connection, debug=debug)
+    return 1
+
+# Funcion para obtener lista de tablas en la base de datos
+def ObtenerTablas(bd: dict, debug: bool = False) -> list | str | None:
+    if debug: print(bd)
+    cursor = CrearCursor(
+        ConectarBD(
+            bd["usuario"],
+            bd["contrasena"],
+            bd["direccion"],
+            bd["puerto"],
+            bd["base_datos"],
+            debug=debug
+        ),
+        debug=debug
+    )
+    try:
+        cursor.execute("SHOW TABLES")
+        if debug: print(f"[bold green]Consulta ejecutada exitosamente[/bold green]")
+        return cursor.fetchall()
+    except mariadb.Error as e:
+        if debug: print(f"[bold red]Error al ejecutar la consulta: {e}[/bold red]")
+        CerrarConexion(cursor.connection, debug=debug)
+        sys.exit(1)
+        return "ERROR"
+
 # Funcion para obtener todos los registros de una tabla
-def ObtenerRegistrosDeTabla(bd: dict, tabla: str = "testing", debug: bool = False):
+def ObtenerRegistrosDeTabla(bd: dict, tabla: str = "testing", debug: bool = False) -> list | int | None:
     if debug: print(bd)
     cursor = CrearCursor(
         ConectarBD(
@@ -74,20 +131,64 @@ def ObtenerRegistrosDeTabla(bd: dict, tabla: str = "testing", debug: bool = Fals
         if debug: print(f"[bold red]Error al ejecutar la consulta: {e}[/bold red]")
         CerrarConexion(cursor.connection, debug=debug)
         sys.exit(1)
+        return "ERROR"
     CerrarConexion(cursor.connection, debug=debug)
-    return cursor.fetchone()
+    return cursor.fetchall()
+
+# Funcion para añadir usuarios a la tabla de usuarios
+def AñadirUsuario(bd: dict, usuario: str, contrasena: str, tipo: int, debug: bool = False) -> None:
+    if debug: print("Insertando usuario a la base...")
+    cursor = CrearCursor(
+        ConectarBD(
+            bd["usuario"],
+            bd["contrasena"],
+            bd["direccion"],
+            bd["puerto"],
+            bd["base_datos"],
+            debug=debug
+        ),
+        debug=debug
+    )
+    try:
+        cursor.execute(
+            "INSERT INTO usuarios (usuario, contrasena, tipo) VALUES (?, ?, ?)",
+            (usuario, contrasena, tipo)
+        )
+        cursor.connection.commit()
+        return 0
+    except mariadb.Error as e:
+        if debug: print(f"[bold red]Error al insertar el usuario: {e}[/bold red]")
+        return 1
+    finally:
+        CerrarConexion(cursor.connection, debug=debug)
 
 # Prueba de las funciones
-print(
-    ObtenerRegistrosDeTabla(
-        bd={
-            "usuario": CONSTANTES["usuario"],
-            "contrasena": CONSTANTES["contrasena"],
-            "direccion": CONSTANTES["direccion"],
-            "puerto": int(CONSTANTES["puerto"]),
-            "base_datos": CONSTANTES["base_datos"]
-        },
-        tabla="testing",
-        debug=True
+def main() -> None:
+    print(
+        ObtenerRegistrosDeTabla(
+            bd={
+                "usuario": CONSTANTES["usuario"],
+                "contrasena": CONSTANTES["contrasena"],
+                "direccion": CONSTANTES["direccion"],
+                "puerto": int(CONSTANTES["puerto"]),
+                "base_datos": CONSTANTES["base_datos"]
+            },
+            tabla="usuarios",
+            debug=True
+        )
     )
-)
+    print(
+        ObtenerTablas(
+            bd={
+                "usuario": CONSTANTES["usuario"],
+                "contrasena": CONSTANTES["contrasena"],
+                "direccion": CONSTANTES["direccion"],
+                "puerto": int(CONSTANTES["puerto"]),
+                "base_datos": CONSTANTES["base_datos"]
+            },
+            debug=True
+        )
+    )
+
+if __name__ == "__main__":
+    main()
